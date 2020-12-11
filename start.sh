5d4abff622d19d479ea7ee99d10bc79b8151394d
Jerry Hall <hall.jerry9@gmail.com>
2020-12-10 17:03:41 -0800

    add start.sh

---
+++ start.sh
@@ -0,0 +1,19 @@
+sudo amazon-linux-extras install docker
+
+echo Starting docker...
+sudo service docker start
+
+IMAGE_NAME="workspace"
+echo Building docker image \"${IMAGE_NAME}\"
+sudo docker build -t ${IMAGE_NAME} .
+
+echo Starting image ${IMAGE_NAME}...
+echo .....To quit, type exit.....
+echo .....To re-connect later, type ./connect.sh
+sudo docker run --name ${IMAGE_NAME} -dt ${IMAGE_NAME} 
+sudo docker exec –it ${IMAGE_NAME} /bin/bash
+
+echo "sudo docker exec –it ${IMAGE_NAME} /bin/bash" > connect.sh
+echo "sudo docker stop ${IMAGE_NAME}" > stop.sh
+echo "./stop.sh; sudo docker rm ${IMAGE_NAME}" > terminate.sh
+chmod +x connect.sh stop.sh terminate.sh
