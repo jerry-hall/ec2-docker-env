f869fadfc1957c6597cf58488cc1d36a94763427
Jerry Hall <hall.jerry9@gmail.com>
2020-12-10 17:07:01 -0800

    add add'tl messaging to start.sh and reorg execution

--- start.sh
+++ start.sh
@@ -9,11 +9,13 @@ sudo docker build -t ${IMAGE_NAME} .
 
 echo Starting image ${IMAGE_NAME}...
 echo .....To quit, type exit.....
-echo .....To re-connect later, type ./connect.sh
+echo .....To re-connect later, type ./connect.sh.....
+echo .....To stop or terminate, type ./stop.sh or ./terminate.....
 sudo docker run --name ${IMAGE_NAME} -dt ${IMAGE_NAME} 
-sudo docker exec –it ${IMAGE_NAME} /bin/bash
 
 echo "sudo docker exec –it ${IMAGE_NAME} /bin/bash" > connect.sh
 echo "sudo docker stop ${IMAGE_NAME}" > stop.sh
 echo "./stop.sh; sudo docker rm ${IMAGE_NAME}" > terminate.sh
 chmod +x connect.sh stop.sh terminate.sh
+
+sudo docker exec –it ${IMAGE_NAME} /bin/bash
